e86a0f5d8264346019ed1e522531fe99e3840993
DIkshant <dikshant.hasbe@nimblework.com>
2025-05-30 14:38:25 +0530

    updated lint and test job

---
+++ src/__tests__/setup.js
@@ -0,0 +1,9 @@
+// Set up different test environments for different test files
+const path = require('path');
+
+// If the test file is in the api directory, use node environment
+if (path.basename(process.env.JEST_WORKER_ID).includes('api.test.js')) {
+    process.env.JEST_ENV = 'node';
+} else {
+    process.env.JEST_ENV = 'jsdom';
+} 
